e38a9e95d1b81c395dc8847bcd974e59ce21e256
unknown <evilnapsis@gmail.com>
2015-08-21 23:30:45 -0700

    Imagen+Descripcion

--- core/modules/index/view/index/widget-default.php
+++ core/modules/index/view/index/widget-default.php
@@ -1,7 +1,9 @@
 <div class="container">
 <div class="row">
-<div class="col-md-12">
+<div class="col-md-8">
 <h1>Diversion por todos lados.</h1>
+<p>SMILE es un sistema de red social para conocer personas y compartir informacion, fotos, videos, etc.</p>
+<img src="res/images/uss.jpg" class="img-responsive">
 </div>
 </div>
 </div>
