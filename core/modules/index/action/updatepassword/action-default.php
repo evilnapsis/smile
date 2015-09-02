b6263cd55e242d5ca6c6906508169f45260838a4
unknown <evilnapsis@gmail.com>
2015-09-01 21:29:55 -0700

    Update password backend

---
+++ core/modules/index/action/updatepassword/action-default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @author evilnapsis
+* @brief Algoritmo para cambiar la contrase~a
+**/
+if(isset($_SESSION["user_id"]) && !empty($_POST)){
+	$user = UserData::getById($_SESSION["user_id"]);
+
+	if($user->password==sha1(md5($_POST["password"])) ){
+		if($_POST["new_password"]==$_POST["confirm_password"]){
+
+			$user->password = sha1(md5($_POST["new_password"]));
+			$user->update_passwd();
+			Core::alert("La contrase~a ha sido actualizada exitosamente!");
+			Core::redir("./?view=configuration");			
+
+		}else{
+			Core::alert("Las contrase~as no coinciden.");
+			Core::redir("./?view=configuration");			
+		}
+
+	}else{
+		Core::alert("La contrase~a introducida es incorrecta.");
+		Core::redir("./?view=configuration");
+	}
+}
+
+
+
+?>
