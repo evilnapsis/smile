f987d5d8486712d088c7d2ee2f7d0a05a337c2c3
unknown <evilnapsis@gmail.com>
2015-09-02 00:44:43 -0700

    Log de cambios v1.0

---
+++ core/modules/index/view/changelog/widget-default.php
@@ -0,0 +1,26 @@
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h2>Log de cambios</h2>
+<p>En esta pagina puedes ver todos los cambios ejercidos en el proyecto <b>smile</b>.</p>
+<h4>v1.0</h4>
+<p><b>Modulos</b></p>
+<ul>
+	<li>Modulo de usuarios</li>
+	<li>Modulo de statuses</li>
+	<li>Modulo de imagenes</li>
+	<li>Modulo de notificaciones</li>
+</ul>
+
+<p><b>Caracteristicas</b></p>
+<ul>
+	<li>Dar like a estados e imagenes</li>
+	<li>Escribir comentarios a estados e imagenes</li>
+	<li>Buscar y ver amigos, solicitud de amistad (enviar y aceptar)</li>
+	<li>Enviar mensajes a amigos</li>
+</ul>
+<p>Y muchas cosas mas implicitas.</p>
+
+</div>
+</div>
+</div>
