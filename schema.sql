f2089bf4e8d9dbdf653c543eddab3eaa577b5c18
Agustin Ramos <evilnapsis@gmail.com>
2016-03-14 10:48:53 -0600

    remove not null from schema.sql

--- schema.sql
+++ schema.sql
@@ -7,25 +7,25 @@ use smile;
 
 create table user(
 	id int not null auto_increment primary key,
-	name varchar(50) not null,
-	lastname varchar(50) not null,
+	name varchar(50),
+	lastname varchar(50),
 	username varchar(50),
-	email varchar(255) not null,
-	password varchar(60) not null,
+	email varchar(255),
+	password varchar(60),
 	code varchar(20),
-	is_active boolean not null default 0,
-	is_admin boolean not null default 0,
-	created_at datetime not null
+	is_active boolean default 0,
+	is_admin boolean default 0,
+	created_at datetime
 );
 
 /* insert into user(email,password,is_active,is_admin,created_at) value ("admin",sha1(md5("admin")),1,1,NOW()); */
 
 create table recover (
 	id int not null auto_increment primary key,
-	user_id int not null,
-	code varchar(20) not null,
-	is_used boolean not null default 0,
-	created_at datetime not null,
+	user_id int,
+	code varchar(20),
+	is_used boolean default 0,
+	created_at datetime,
 	foreign key(user_id) references user(id)
 );
 
@@ -76,24 +76,24 @@ create table profile(
 
 create table album(
 	id int not null auto_increment primary key,
-	title varchar(200) not null,
-	content varchar(500) not null,
-	user_id int not null,
-	level_id int not null,
-	created_at datetime not null,
+	title varchar(200),
+	content varchar(500),
+	user_id int,
+	level_id int,
+	created_at datetime,
 	foreign key (user_id) references user(id),
 	foreign key (level_id) references level(id)
 );
 
 create table image(
 	id int not null auto_increment primary key,
-	src varchar(255) not null,
-	title varchar(200) not null,
-	content varchar(500) not null,
-	user_id int not null,
-	level_id int not null,
+	src varchar(255),
+	title varchar(200),
+	content varchar(500),
+	user_id int,
+	level_id int,
 	album_id int,
-	created_at datetime not null,
+	created_at datetime,
 	foreign key (album_id) references album(id),
 	foreign key (user_id) references user(id),
 	foreign key (level_id) references level(id)
@@ -107,23 +107,23 @@ create table image(
 create table post(
 	id int not null auto_increment primary key,
 	title varchar(500) ,
-	content text not null,
+	content text,
 	lat double ,
 	lng double ,
 	start_at datetime,
 	finish_at datetime,
-	receptor_type_id int not null default 1, /* 1.- user, 2.- group **/
-	author_ref_id int not null,
-	receptor_ref_id int not null,
-	level_id int not null,
+	receptor_type_id int default 1, /* 1.- user, 2.- group **/
+	author_ref_id int,
+	receptor_ref_id int,
+	level_id int,
 	post_type_id int default 1,
-	created_at datetime not null,
+	created_at datetime,
 	foreign key (level_id) references level(id)
 );
 
 create table post_image(
-	post_id int not null,
-	image_id int not null,
+	post_id int,
+	image_id int,
 	foreign key (post_id) references post(id),
 	foreign key (image_id) references image(id)
 );
@@ -135,52 +135,52 @@ create table post_image(
 **/
 create table heart(
 	id int not null auto_increment primary key,
-	type_id int not null default 1,
-	ref_id int not null,
-	user_id int not null,
-	created_at datetime not null,
+	type_id int default 1,
+	ref_id int,
+	user_id int,
+	created_at datetime,
 	foreign key (user_id) references user(id)
 );
 
 create table comment(
 	id int not null auto_increment primary key,
-	type_id int not null,
-	ref_id int not null,
-	user_id int not null,
-	content text not null,
+	type_id int,
+	ref_id int,
+	user_id int,
+	content text,
 	comment_id int,
-	created_at datetime not null,
+	created_at datetime,
 	foreign key (user_id) references user(id),
 	foreign key (comment_id) references comment(id)
 );
 
 create table friend(
 	id int not null auto_increment primary key,
-	sender_id int not null,
+	sender_id int,
 	receptor_id int,
-	is_accepted boolean not null default 0,
-	is_readed boolean not null default 0,
-	created_at datetime not null,
+	is_accepted boolean default 0,
+	is_readed boolean default 0,
+	created_at datetime,
 	foreign key (sender_id) references user(id),
 	foreign key (receptor_id) references user(id)
 );
 
 create table conversation(
 	id int not null auto_increment primary key,
-	sender_id int not null,
+	sender_id int,
 	receptor_id int,
-	created_at datetime not null,
+	created_at datetime,
 	foreign key (sender_id) references user(id),
 	foreign key (receptor_id) references user(id)
 );
 
 create table message(
 	id int not null auto_increment primary key,
-	content text not null,
-	user_id int not null,
+	content text,
+	user_id int,
 	conversation_id int,
-	created_at datetime not null,
-	is_readed boolean not null default 0,
+	created_at datetime,
+	is_readed boolean default 0,
 	foreign key (user_id) references user(id),
 	foreign key (conversation_id) references conversation(id)
 );
@@ -193,13 +193,13 @@ type:
 */
 create table notification(
 	id int not null auto_increment primary key,
-	not_type_id int not null,
-	type_id int not null,
-	ref_id int not null,
-	receptor_id int not null,
-	sender_id int not null,
-	is_readed boolean not null default 0,
-	created_at datetime not null,
+	not_type_id int,
+	type_id int,
+	ref_id int,
+	receptor_id int,
+	sender_id int,
+	is_readed boolean default 0,
+	created_at datetime,
 	foreign key (sender_id) references user(id),
 	foreign key (receptor_id) references user(id)
 );
@@ -208,10 +208,10 @@ create table notification(
 create table team (
 	id int not null auto_increment primary key,
 	image varchar(200),
-	title varchar(200) not null,
+	title varchar(200),
 	description varchar(500) ,
-	user_id int not null,
-	status int not null default 1 /* 1.- open, 2.- closed */,
-	created_at datetime not null,
+	user_id int,
+	status int default 1 /* 1.- open, 2.- closed */,
+	created_at datetime,
 	foreign key (user_id) references user(id)
 );
